5141c1b746f662416ae1f6432ea8c05492aa9996
Tomik David <tomik.david1@gmail.com>
2023-11-23 08:13:05 +0100

    Commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,8 +6,34 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  @Render('urlap')
+  geturlap( 
+    @Query('email') email: string,
+    @Query('jelszo')  jelszo: string,
+    @Query('jelszo2')  jelszo2: string,
+  ): object {
+    let pattern = /[a-zA-Z1-9]/;
+    let pattern2 = /[a-z@.]/;
+    const errors: any = {};
+    if (email === '') {
+      errors.email = 'A email mezőt is töltsd ki!';
+    }
+    else if ((new RegExp(pattern2)).test(email)) {
+      errors.email2 = 'Az emailnek tartalmaznia kell @ és .-ot';
+    }
+    if (jelszo === '') {
+      errors.jelszo = 'A jelszo mezőt is töltsd ki!';
+    }
+    else if ((new RegExp(pattern)).test(jelszo)) {
+      errors.jelszo3 = 'A jelszonak tartalmaznia kell kis és nagy betüt és számot';
+    }
+    if (jelszo2 === '') {
+      errors.jelszo2 = 'A Jelszo ismetles mezőt is töltsd ki!';
+    }
+    else if (jelszo2 !== jelszo) {
+      errors.jelszo4 = 'A jelszok nem egyeznek meg';
+    }
+    //return { nev: nev, eletkor: eletkor, errors: errors };
+    return { email, jelszo, jelszo2, errors, negyedik: 12 };
   }
 }
